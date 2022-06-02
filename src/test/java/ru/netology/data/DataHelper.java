6762014b516c911970d9b47f929e2d50ae3079a6
Artem-Mv <Stimul113@gmail.com>
2022-06-02 21:36:31 +0700

    push init

---
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -0,0 +1,32 @@
+package ru.netology.data;
+
+import lombok.Value;
+
+public class DataHelper {
+    public DataHelper() {
+    }
+
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
+
+    public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
+        return new VerificationCode("12345");
+    }
+
+    @Value
+    public static class CardInfo {
+        private String cardNumber;
+    }
+}
